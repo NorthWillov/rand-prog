ac77603a85ff914682ad5cd3a7202e829508b18f
NorthWillov <northwillov@gmail.com>
2023-04-28 13:54:12 +0200

    Add progs

--- src/oldProgsDb.js
+++ src/oldProgsDb.js
@@ -1,2 +1,9 @@
 // !!!IMPORTANT ONLY UPPERCASE!!!
-export default ["PRO_Z22_MISJA", "PRO_UPO_W23_KWIECIEN", "ITME_105"];
+export default [
+  "PRO_Z22_MISJA",
+  "PRO_UPO_W23_KWIECIEN",
+  "ITME_105",
+  "PRO_Z23_1procent35",
+  "PRO_Z23_1procent37",
+  "PRO_Z23_1procent15_2",
+];
